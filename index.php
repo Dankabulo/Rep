0f9b20e6f3ddb47444290e397f34bd3806cacb33
Dan kabulo <16km081@esisalama.org>
2018-11-06 08:35:32 +0100

    creation du fichier index.php avec son formulaire de connexion

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>ExpliqueSimplement
+        </title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <h2>ExpliqueSimplement</h2>
+    <div>    Se connecter
+        </br></br>
+        <form method="post" action="connexion.php">
+            <label>Psuedo :</label>
+                <input type="text" name="pseudo" placeholder="Entrez votre Pseudo" /> <br/><br/>
+            <label>Mot de passe :</label>
+                <input type="password" name="pwd" class="form-control" placeholder="Mot de passe"></br></br>
+            
+            <input type="submit" value="connecter" /><br/>
+            <a href="account.php">
+            </br>    
+            Creer un compte</a></br>
+            <a href="bdd.php">j'ai oublie mon mot de passe</a>;
+        </form>
+    </div>
+    </body>
+
+
+</html>
